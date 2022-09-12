e90f6849a366d48ef3c3270dee2a8ecaa80773a9
SumayyaGitHub <93243336+SumayyaGitHub@users.noreply.github.com>
2022-09-12 22:54:42 +0530

    Add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-import { NavComponent } from './component/shared/nav/nav.component';
-import { HeaderComponent } from './component/shared/header/header.component';
+import { NavComponent } from './component/nav/nav.component';
+import { HeaderComponent } from './component/header/header.component';
 import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './component/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './component/shopping-cart/product-list/product-list.component';
+import { DialogComponent } from './component/shopping-cart/filters/dialog/dialog.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   declarations: [
     AppComponent,
     HelloComponent,
     NavComponent,
     HeaderComponent,
     ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    DialogComponent,
   ],
   bootstrap: [AppComponent],
 })
